d3c446d03a2bd00c4c4812f4420b057bfa2f0cfa
Chandra Pratap Singh <sumitsnghtmr@gmail.com>
2026-07-01 01:38:47 +0530

    Update API URL for task management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import TaskFilter from "./components/TaskFilter";
 import bgImage from "./assets/task2.png";
 
 const API_URL =
-    "https://task-management-web-application-production.up.railway.app/api/tasks";
+    "https://task-management-web-application-m07y.onrender.com";
 
 function App() {
     const [tasks, setTasks] = useState([]);
